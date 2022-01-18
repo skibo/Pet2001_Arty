dca8f1859d4329b32a8ee5b909c5d75d031924e6
Thomas Skibo <thomas-git@skibo.net>
2022-01-18 15:24:20 -0800

    Update project.tcl for later Vivado versions.

--- project.tcl
+++ project.tcl
@@ -126,6 +126,7 @@ set proj_dir [get_property directory [current_project]]
 
 # Set project properties
 set obj [current_project]
+set_property -name "board_part_repo_paths" -value "[file normalize "~/.Xilinx/Vivado/2021.2/xhub/board_store/xilinx_board_store"] [file normalize "$origin_dir/../.Xilinx/Vivado/2020.2/xhub/board_store/xilinx_board_store"]" -objects $obj
 set_property -name "board_part" -value "digilentinc.com:arty:part0:1.1" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "enable_vhdl_2008" -value "1" -objects $obj
